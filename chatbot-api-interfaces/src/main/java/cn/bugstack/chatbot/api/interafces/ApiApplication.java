831aff1317f4d82a41551f8180db0c4b2da8b1e6
zj <15314154287@163.com>
2024-12-17 15:25:36 +0800

    小傅哥,feat: init ddd project

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/interafces/ApiApplication.java
@@ -0,0 +1,17 @@
+package cn.bugstack.chatbot.api.interafces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zj
+ * @description 启动入口
+ * @github
+ * @Copyright
+ */
+@SpringBootApplication
+public class ApiApplication{
+    public static void main( String[] args ){
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/interafce/ApiTest.java
@@ -0,0 +1,12 @@
+package cn.bugstack.chatbot.api.interafce;
+
+/**
+ * Unit test for simple App.
+ */
+
+/**
+ *
+ */
+public class ApiTest{
+
+}
